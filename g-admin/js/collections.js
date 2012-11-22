eb80a50cd2ad9cfe5922f0dc3224a48b97e2abac
Sam Schooler <schooler.sam@gmail.com>
2012-11-21 18:41:17 -0600

    Added absolute model getter

--- g-admin/js/collections.js
+++ g-admin/js/collections.js
@@ -13,7 +13,17 @@ Ghost.Collections._Posts = Backbone.Collection.extend({
 		post.set({active: true});
 		this.trigger("update_posts");
 	},
+	set_active_at: function(id){
+		var post = this.at(id);
+		this.clear_active();
+		post.set({active: true});
+		this.trigger("update_posts");
+	},
 	get_active: function(){
-		return this.where({active: true})[0].get("id");
+		var active = this.where({active: true});
+		if(active.length > 0)
+			return active[0].get("id");
+		else
+			return false;
 	}
 });
