1639b772632703f9ca036ac19a57caa3556a221e
Sam Schooler <schooler.sam@gmail.com>
2012-11-23 19:54:53 -0600

    Added a longer paragraph for debugging

--- g-admin/js/posts.js
+++ g-admin/js/posts.js
@@ -36,7 +36,7 @@ var Ghost = {
 			tags: ["new", "hot", "sexy"],
 			date: "12/3/1996",
 			view: 3123,
-			content: "*Content* goes here for article 2.", 
+			content: "From this day forward, Flight Control will be known by two words: 'Tough' and 'Competent.' Tough means we are forever accountable for what we do or what we fail to do. We will never again compromise our responsibilities. Every time we walk into Mission Control we will know what we stand for. Competent means we will never take anything for granted. We will never be found short in our knowledge and in our skills. Mission Control will be perfect. When you leave this meeting today you will go to your office and the first thing you will do there is to write 'Tough and Competent' on your blackboards. It will never be erased. Each day when you enter the room these words will remind you of the price paid by Grissom, White, and Chaffee. These words are the price of admission to the ranks of Mission Control.", 
 			published: true, 
 			featured: true
 		});
