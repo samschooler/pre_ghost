545b2c8fddd3d8d780b9ae73aee27a9309da5914
Sam Schooler <schooler.sam@gmail.com>
2012-12-09 15:33:35 -0600

    Fixed tags

--- g-admin/js/posts.js
+++ g-admin/js/posts.js
@@ -37,7 +37,7 @@ var Ghost = {
 		// !Debug
 		var post = new Ghost.Models._Post({
 			title: "Two.post",
-			tags: ["new", "hot", "sexy"],
+			tags: ["cats", "puppies", "projects"],
 			date: "12/3/1996",
 			view: 3123,
 			content: "The view of the Earth from the Moon fascinated me—a small disk, 240,000 miles away. It was hard to think that that little thing held so many problems, so many frustrations. Raging nationalistic interests, famines, wars, pestilence don't show from that distance.", 
@@ -48,7 +48,7 @@ var Ghost = {
 
 		var post_one = new Ghost.Models._Post({
 			title: "One.post",
-			tags: ["new", "hot", "sexy"],
+			tags: ["projects", "cool", "awesome"],
 			date: "12/3/1996",
 			view: 3123,
 			content: "From this day forward, **Flight** Control will be known by two words: ‘Tough’ and ‘Competent.’ Tough means we are forever accountable for what we do or what we fail to do. We will never again compromise our responsibilities. Every time we walk into Mission Control we will know what we stand for. Competent means we will never take anything for granted. We will never be found short in our knowledge and in our skills. Mission Control will be perfect. When you leave this meeting today you will go to your office and the first thing you will do there is to write ‘Tough and Competent’ on your blackboards. It will never be erased. Each day when you enter the room these words will remind you of the price paid by Grissom, White, and Chaffee. These words are the price of admission to the ranks of Mission Control.", 
